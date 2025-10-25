7b6bf28faf90496de32aeff81430a4432fcd93ad
MoEhab74 <mohamedehabswe74@gmail.com>
2025-10-25 19:23:48 +0300

    Violate ISP by adding new feature to the Iorder Interface

--- lib/ISP/iorder.dart
+++ lib/ISP/iorder.dart
@@ -2,5 +2,6 @@ abstract class IOrder {
   void chashOrderInvoice();
   void creditOrderInvoice();
   void onLineOrderInvoice();
+  // When u add any new method here, all implementing classes must implement it ===> Violation of ISP
   void digitalOrderInvoice();
 }
